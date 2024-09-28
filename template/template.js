7b042ee93dbb56755ecdd767bbac23df4b07ebf8
Leonardo Baldo <baldoleonardo01@gmail.com>
2024-09-28 11:47:13 +0200

    template

---
+++ template/template.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('template/header.html')
+    	.then(response => response.text())
+		.then(html => {
+			const headerContainer = document.getElementById('header');
+			headerContainer.innerHTML = html;
+		})
+		.catch(error => {
+			console.error('Error loading header:', error);
+		});
+	
+	fetch('template/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            const footerContainer = document.getElementById('footer');
+            footerContainer.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading footer:', error);
+        });
+});
