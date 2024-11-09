892fdc5f9021b05acdff2fddaecfa39402391c50
Leonardo Baldo <baldoleonardo01@gmail.com>
2024-11-09 22:07:30 +0100

    TopFirst

--- step/step.js
+++ step/step.js
@@ -6,6 +6,7 @@ const files = [
 	{ filePath: 'step/CLL.json', id: 'CLL' },
 	{ filePath: 'step/EG1.json', id: 'EG1' },
 	{ filePath: 'step/EG2.json', id: 'EG2' },
+	{ filePath: 'step/TopFirst.json', id: 'TopFirst' },
 	{ filePath: 'step/LLC.json', id: 'LLC' },
 ];
   
